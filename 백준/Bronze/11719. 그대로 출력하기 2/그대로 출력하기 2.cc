46f56e7b096f228160fc23956b59ec1bb4278f83
GongMinGi <76468280+GongMinGi@users.noreply.github.com>
2026-02-12 01:52:23 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0); 
+	cin.tie(0);
+
+	string temp;
+
+	while (getline(cin, temp))
+	{
+		cout << temp << endl;
+	}
+
+	return 0;
+}
