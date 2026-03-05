adeef0b5aff0af53c1ef12a2544f837742d2cb0e
GongMinGi <76468280+GongMinGi@users.noreply.github.com>
2026-03-06 02:05:45 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c;
+    cin >> d;
+
+    a += d / 3600;
+    d = d % 3600;
+    b += d / 60;
+    d = d % 60;
+    c += d;
+
+    if (c >= 60)
+    {
+        c -= 60;
+        b += 1;
+    }
+    if (b >= 60)
+    {
+        b -= 60;
+        a += 1;
+    }
+    if (a >= 24)
+    {
+        a %= 24;
+    }
+    cout << a << " " << b << " " << c;
+    return 0;
+}
