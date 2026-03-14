d99361a9d7ad3b97c2f61642b2d3dc8da281c8c6
GongMinGi <76468280+GongMinGi@users.noreply.github.com>
2026-03-14 16:33:45 +0900

    [Gold IV] Title: 행렬 제곱, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
@@ -1,117 +1,92 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#define MOD 1000
+#include <algorithm>
+
+#define Mod 1000
 
 using namespace std;
 
 int n;
 long long b;
-int nMatrix[5][5];
-int resultMat[5][5];
 
-void MatrixPower()
+struct Matrix
 {
-	int tempMat[5][5];
+	int mat[5][5];
 
-	for (int i = 0; i < n; i++)
+	// 기본생성자: 모든 원소를 0으로 초기화
+	Matrix()
 	{
-		for (int j = 0; j < n; j++)
+		for (int i = 0; i < n; i++)
 		{
-			int temp = 0;
-			for (int k = 0; k < n; k++)
+			for (int j = 0; j < n; j++)
 			{
-				temp += ((nMatrix[i][k] % MOD) * (nMatrix[k][j] % MOD)) % MOD;
+				mat[i][j] = 0;
 			}
-			tempMat[i][j] = temp % MOD;
 		}
 	}
 
-	for (int i = 0; i < n; i++)
+	//곱셈 연산자 오버로딩
+	// 행렬끼리 *을 통해 곱할때 어떻게 동작할지 정의
+	Matrix operator*(const Matrix& other) const
 	{
-		for (int j = 0; j < n; j++)
-		{
-			nMatrix[i][j] = tempMat[i][j];
-		}
-	}
-}
+		Matrix result;
 
-void MatrixProductToResult()
-{
-	int tempMat[5][5];
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
+		for (int i = 0; i < n; i++)
 		{
-			int temp = 0;
-			for (int k = 0; k < n; k++)
+			for (int j = 0; j < n; j++)
 			{
-				temp += ((resultMat[i][k] % MOD) * (nMatrix[k][j] % MOD)) % MOD;
+				int temp;
+				for (int k = 0; k < n; k++)
+				{
+					result.mat[i][j] += mat[i][k] * other.mat[k][j];
+					result.mat[i][j] = result.mat[i][j] % Mod;
+				}
 			}
-			tempMat[i][j] = temp % MOD;
 		}
+
+		return result;
 	}
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			resultMat[i][j] = tempMat[i][j];
-		}
-	}
-}
-
-void DFS(long long power)
-{
-
-	if (power != 1 && power % 2 == 1)
-	{
-		MatrixProductToResult();
-	}
-
-	if (power == 1)
-	{
-		MatrixProductToResult();
-		return;
-	}
-
-	MatrixPower();
-	DFS(power / 2);
-
-	return;
-}
-
+};
 
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> n >> b;
+	cin >> n >> b; 
+
+	Matrix nMat;
+	Matrix resultMat;
 
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			cin >> nMatrix[i][j];
-			if (i == j)
-				resultMat[i][j] = 1;
-			else
-				resultMat[i][j] = 0;
+			cin >> nMat.mat[i][j];
 		}
+		resultMat.mat[i][i] = 1;		// 대각선 성분만 1로 설정하여 단위행렬 생성
 	}
 
-	DFS(b);
+	// 거듭제곱 로직
+	// while문을 통해 함수호출 오버헤드 제거
+	while (b > 0)
+	{
+		if (b %2 == 1)
+		{
+			resultMat = resultMat * nMat;	// 홀수일때만 결과에 nMat을 곱함
+		}
+		nMat = nMat * nMat;					// 행렬을 제곱하고 지수를 반으로 나눔
+		b /= 2;
+	}
 
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			cout << resultMat[i][j] << " ";
+			cout << resultMat.mat[i][j] << " ";
 		}
 		cout << "\n";
 	}
 
 	return 0;
-
 }
