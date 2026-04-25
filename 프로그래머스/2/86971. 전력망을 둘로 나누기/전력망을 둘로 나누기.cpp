b12d22c706c5e16cb54a872b362d9e7d40a0ad98
GongMinGi <76468280+GongMinGi@users.noreply.github.com>
2026-04-25 17:53:38 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.14 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,73 +1,9 @@
-#include <iostream>
-#include <algorithm>
-#include <queue>
 #include <string>
 #include <vector>
-#include <cstring>
 
 using namespace std;
 
-vector<vector<int>> edges(101);
-bool visitCheck[101];
-
-int BFS(int ignoreA, int ignoreB)
-{
-    fill(visitCheck, visitCheck + 101, false);
-    queue<int> q;
-    q.push(ignoreA);
-    visitCheck[ignoreA] = true;
-    int nodeCnt =1 ;
-    
-    while(q.empty() == false)
-    {
-        int curNode = q.front();
-        q.pop();
-        
-        for(auto nextNode :edges[curNode])
-        {
-            if( (curNode == ignoreA && nextNode == ignoreB) ||
-                (curNode == ignoreB && nextNode == ignoreA))
-            {
-                continue;
-            }
-            
-            else if (visitCheck[nextNode] == true) continue;
-            
-            q.push(nextNode);
-            visitCheck[nextNode] = true;
-            nodeCnt++;
-        }
-    }
-    
-    return nodeCnt;
-}
-
 int solution(int n, vector<vector<int>> wires) {
-    int answer = 101;
-    
-    for (int i =0; i<wires.size(); i ++)
-    {
-        int start = wires[i][0];
-        int end = wires[i][1];
-        
-        edges[start].push_back(end);
-        edges[end].push_back(start);
-    }
-    
-    
-    for(int i = 0; i <wires.size() ; i ++)
-    {
-        int a = wires[i][0];
-        int b = wires[i][1];
-        
-        int partA = BFS(a, b);
-        int partB = n- partA;
-        
-        answer = min ( answer, abs(partA - partB));
-        
-    }
-    
-    
-    
+    int answer = -1;
     return answer;
 }
