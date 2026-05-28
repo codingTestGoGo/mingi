d75f134f85331c76df4510c7fb0a7e94e6396c31
GongMinGi <76468280+GongMinGi@users.noreply.github.com>
2026-05-29 02:04:59 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 3.94 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -3,17 +3,40 @@
 #include <string>
 #include <vector>
 
+// 1, 5, 25, 125, 625
+// 1, 6, 31, 156, 781
+
 using namespace std;
-string vowel = "AEIOU";
+
+int weight[5] = { 781, 156, 31, 6, 1 };
 
 int solution(string word) {
-    int answer = 0;
-    int a = word.size();
+    int answer;
     
-    for(int i =0, b = 1; i<word.size() ; i++, b*=5)
+    for(int i = 0; i <word.size(); i++)
     {
-        a += vowel.find(word[i]) * 781 / b;
+        int current;
+        switch(word[i])
+        {
+            case 'A':
+                current = 0;
+                break;
+            case 'E':
+                current = 1;
+                break;
+            case 'I':
+                current = 2;
+                break;
+            case 'O':
+                current = 3;
+                break;
+            case 'U':
+                current = 4;
+                break;
+        }
+        
+        answer += current * weight[i];
     }
     
-    return a;
+    return answer;
 }
