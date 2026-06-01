985c2c8647d926a19f3dd274aa7003553444375f
GongMinGi <76468280+GongMinGi@users.noreply.github.com>
2026-06-02 04:23:52 +0900

    [level 2] Title: 소수 찾기, Time: 27.89 ms, Memory: 4.39 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -1,78 +1,67 @@
-#include <iostream>
 #include <string>
 #include <vector>
-#include <cstring>
-#include <stdlib.h>
+#include <iostream>
 #include <algorithm>
+#include <math.h>
+#include <set>
 
 using namespace std;
+    
+int answer = 0;
+set<int> primeNumbers;
 
-bool isPrime [10000000];
+void DFS(string numbers, vector<bool>& isPrime, string curString, vector<bool>& isVisit)
+{
+    cout << curString << endl; 
+    if(curString != "")
+    {
+        int curNum = stoi(curString);
+        if (isPrime[curNum] == true && primeNumbers.find(curNum) == primeNumbers.end()) 
+        {
+            cout << curNum << endl;
+            primeNumbers.insert(curNum);
+        }
+    }
+    
+    if (curString.length() == numbers.length()) return;
+    
+    for(int i = 0; i <numbers.size(); i++)
+    {
+        if(isVisit[i] == false)
+        {
+            isVisit[i] = true;
+            DFS(numbers, isPrime, curString + numbers[i], isVisit); 
+            isVisit[i] = false;
+        }
+    }
+}
 
 int solution(string numbers) {
-    int answer = 0;
-    memset(isPrime, true, 10000000);
-    for(int i = 2 ; i < 3162; i++)
+    sort(numbers.begin(), numbers.end(), greater<char>());
+    int n = 9999999;
+    int sqrtN = sqrt(n);
+    vector<bool> isPrime(n+1, true);
+    vector<bool> isVisit(numbers.size(), false);
+    
+    isPrime[0] = isPrime[1] = false;
+    for(int i = 2; i <sqrtN ;i ++)
     {
         if(isPrime[i] == true)
         {
-            for(int j = i*2; j<10000000; j+= i)
+            //cout << i << endl;
+            for(int j = i * 2 ; j <isPrime.size(); j +=i)
             {
                 isPrime[j] = false;
             }
         }
     }
-    sort(numbers.begin(), numbers.end(), greater<char>());
-    int maxNumber = stoi(numbers);
     
-    cout << isPrime[9999999];
+    DFS(numbers, isPrime, "", isVisit);
+    answer = primeNumbers.size();
     
-    string targetStr;
-    
-    for(int curNum = 2; curNum < 10000000; curNum++)
-    {
-        if(isPrime[curNum] == false) continue;
-        if(curNum > maxNumber) break;
-        targetStr = to_string(curNum);
-        
-        sort(targetStr.begin(), targetStr.end(), greater<char>());
-        
-        int numPtr = 0;
-        int primePtr = 0;
-        
-        while(1)
-        {
-            if(primePtr == targetStr.length())
-            {
-                answer++;
-                break;
-            }
-            else if (numPtr == numbers.length())
-            {
-                break;
-            }
-            
-            if (numbers[numPtr] == targetStr[primePtr])
-            {
-                primePtr++;
-                numPtr++;
-                continue;
-            }
-            else if (numbers[numPtr] != targetStr[primePtr])
-            {
-                numPtr++;
-                continue;
-            }
-        }
-    }
-    
-    return answer;
+    return answer; 
 }
 
-
-// 천만 에라체 제작
-// numbers 내림차순정렬
-// 그거보다 낮은 소수 리스트업
-// 소수를 스트링화
-// 스트링 원소를 numbers가 모두 갖고 잇는지 대비
-// 전부 오름차순 정렬해서 인덱스 초기화 안하고 대비해서 대비가 모두 통과되면 answer ++ ;
+// 가장 큰 수 의 루트 크기의 에라체를 만들어서 선형탐색
+// char 벡터를 쓰면 빼고 끼는게 자유롭다 다만 int 화시킬때 어렵다
+// string을 쓰면 인트화시키가 쉬운데 문자 하나를 빼는게 어렵다
