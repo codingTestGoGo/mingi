071f273cc4448a3c16a148a82d2a46c66616c0eb
GongMinGi <76468280+GongMinGi@users.noreply.github.com>
2026-06-03 04:08:25 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 3.94 MB, 시간: 0.01 ms
+메모리: 4.13 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 29일 02:04:55
+2026년 06월 03일 04:08:22
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -1,42 +1,34 @@
-#include <iostream>
-#include <algorithm>
 #include <string>
 #include <vector>
 
-// 1, 5, 25, 125, 625
-// 1, 6, 31, 156, 781
-
 using namespace std;
 
-int weight[5] = { 781, 156, 31, 6, 1 };
+int weight[5] = {781, 156, 31, 6, 1};
 
 int solution(string word) {
-    int answer;
-    
-    for(int i = 0; i <word.size(); i++)
+    int answer = 0;
+    answer += word.length();
+    for(int i = 0; i <word.length(); i ++)
     {
-        int current;
         switch(word[i])
         {
             case 'A':
-                current = 0;
+                answer += weight[i] * 0;
                 break;
             case 'E':
-                current = 1;
+                answer += weight[i] * 1;
                 break;
             case 'I':
-                current = 2;
+                answer += weight[i] * 2;
                 break;
             case 'O':
-                current = 3;
+                answer += weight[i] * 3;
                 break;
             case 'U':
-                current = 4;
+                answer += weight[i] * 4;
                 break;
         }
-        
-        answer += current * weight[i];
     }
     
-    return answer;
+    return answer; 
 }
