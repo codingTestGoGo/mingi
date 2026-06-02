2bd5d525e53f3f342407854be4465a357ce1c5c8
GongMinGi <76468280+GongMinGi@users.noreply.github.com>
2026-06-03 03:07:15 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 1.10 ms, Memory: 3.95 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.cpp
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.cpp
@@ -1,61 +1,75 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 #include <unordered_map>
+#include <set>
 
 using namespace std;
 
-unordered_map <string, int> map;
+set<string> allCourses;
+unordered_map<string, int> overlapedCourse;
 
-// 중복없이 모든 오름차순 조합 구하기
-void DFS(string order, string temp, int idx)
+void DFS(string order, string curCombination, int curCourse, int idx)
 {
-    if(temp.size() > order.size()) return;
-    
-    map[temp]++;
-    for(int i = idx; i <order.size() ; i++)
+    if(curCombination.length() == curCourse)
     {
-        DFS(order, temp+order[i], i + 1); 
+        // 없으면  
+        if (allCourses.find(curCombination)== allCourses.end())
+        {
+            allCourses.insert(curCombination);
+        }
+        else
+        {
+            overlapedCourse[curCombination]++;
+        }
+        return;
     }
     
+    for(int i = idx; i <order.length() ; i++)
+    {
+        DFS(order, curCombination + order[i], curCourse, i+1);
+    }
 }
 
 vector<string> solution(vector<string> orders, vector<int> course) {
     vector<string> answer;
     
-    for(auto order : orders)
+    for(int i = 0;i <orders.size(); i ++)
     {
-        sort(order.begin(), order.end());
-        DFS(order, "", 0);
-    }
-    for(auto courseOrder : course)
-    {
-        int mostOrdered = 0;
+        sort(orders[i].begin(), orders[i].end());
         
-        for(auto order: map)
+        cout <<orders[i] << endl;
+    }
+    for(int i = 0; i <course.size(); i++)
+    {
+        for(auto order: orders)
         {
-            if(order.first.length() != courseOrder) continue;
-            mostOrdered = max(mostOrdered, order.second);
+            DFS(order, "", course[i], 0);
         }
         
-        if (mostOrdered < 2) continue;
-        
-        for (auto order: map)
+        int maxCnt = 0;
+        for(auto course: overlapedCourse)
         {
-            if(order.second == mostOrdered && order.first.length() == courseOrder)
+            maxCnt = max(maxCnt, course.second);
+        }
+        for(auto course: overlapedCourse)
+        {
+            if (course.second == maxCnt)
             {
-                answer.push_back(order.first);
+                answer.push_back(course.first);
             }
         }
+        
+        allCourses.clear();
+        overlapedCourse.clear();
     }
-    
+
     sort(answer.begin(), answer.end());
     
     return answer;
 }
 
-/*
-order 들을 정렬한다.
-각 order로 만들 수 있는 모든 조합을 map에 저장한다.
-course의 숫자마다 가장 많은 주문횟수를 가지는 코스를 answer에 넣는다.
-*/
+// course 와 길이가 똑같은 것들중에, 중복되는 문자열을 넣어야한다.
+// 길이가 같은 모든 문자열들을 만들고 unorderedmap에 넣는다.
+// 이걸
