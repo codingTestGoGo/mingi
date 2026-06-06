4989ada6ac3ecf3fa8177008b626fa4072570618
GongMinGi <76468280+GongMinGi@users.noreply.github.com>
2026-06-06 23:04:30 +0900

    [level 3] Title: 단속카메라, Time: 0.47 ms, Memory: 3.88 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -4,53 +4,23 @@
 
 using namespace std;
 
-bool Compare(vector<int>& a , vector<int>& b)
+bool Compare(vector<int>& a, vector<int>& b) 
 {
-    if (a[0] == b[0]) return a[1] < b[1];
-    else return a[0] <b[0];
+    return a[1] <= b[1];
 }
 
 int solution(vector<vector<int>> routes) {
     int answer = 0;
-    vector<pair<int, int>> criticalSection;
-    
+    int camera = -30001;
     sort(routes.begin(), routes.end(), Compare);
     
     for(auto route : routes)
     {
-        int routeStart = route[0];
-        int routeEnd = route[1];
-        bool newSectionNeed = true;
-        
-        // 교차검사
-        for(int i = 0; i < criticalSection.size(); i++)
+        if(route[0] > camera)
         {
-            int secStart = criticalSection[i].first;
-            int secEnd = criticalSection[i].second;
-            
-            // 겹치지 않을경우
-            if(routeEnd < secStart || routeStart > secEnd)
-            {
-                continue;
-            }
-            else
-            {
-                criticalSection[i].first = max(routeStart, secStart);
-                criticalSection[i].second = min(routeEnd, secEnd);
-                newSectionNeed = false;
-                break;
-            }
+            answer++;
+            camera = route[1];
         }
-        
-        if(newSectionNeed == true)
-        {
-            criticalSection.push_back({routeStart, routeEnd});
-        }
-        
     }
-    
-    answer = criticalSection.size();
-    
-    
     return answer;
 }
