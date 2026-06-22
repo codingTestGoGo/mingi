61c13d6c3890cfb1ce0eae80d16c4d18205767e2
GongMinGi <76468280+GongMinGi@users.noreply.github.com>
2026-06-22 19:31:09 +0900

    [level 3] Title: 아이템 줍기, Time: 0.36 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
@@ -1,131 +1,87 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <algorithm>
+#include <tuple>
 #include <iostream>
 
 using namespace std;
 
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
+int dx[4] = { 1, 0, -1, 0};
+int dy[4] = { 0, 1, 0, -1};
 
-void BFS(vector<vector<int>>& board, vector<vector<int>>& dist, int startX, int startY, int targetX, int targetY)
+bool CanGo(vector<vector<int>>& rectangle, int a, int b)
 {
-    queue<pair<int,int>> q;
-    q.push({startX, startY});
-    dist[startX][startY] = 0;
+    //cout << a << " " << b <<endl;
+    bool isOnLine = false;
     
-    while(!q.empty())
-    {
-        int curX = q.front().first;
-        int curY = q.front().second;
-        q.pop();
-        
-        for(int i = 0; i <4; i ++)
-        {
-            int nx = curX + dx[i];
-            int ny = curY + dy[i];
-            
-            if(nx <1 || ny <1 || nx >100 || ny >100) continue;
-            
-            // 방문한 적이 없고, 테두리 위의 점인경우
-            if (dist[nx][ny] == -1 && board[nx][ny] == 1)
-            {
-                dist[nx][ny] = dist[curX][curY] + 1;
-                q.push({nx, ny});
-            }
-        }
-        
-    }
-}
-
-// 사각형의 테두리르 먼저 그리고 겹치는 내부 테두리를 2로 채운다
-void BoardSetting(vector<vector<int>>& rectangle, vector<vector<int>>& board)
-{
     for(auto rect : rectangle)
     {
-        int x1 = rect[0] * 2;
-        int y1 = rect[1] * 2;
-        int x2 = rect[2] * 2; 
-        int y2 = rect[3] * 2;
-        
-        for(int i = x1; i <= x2 ; i++)
-        {
-            for(int j = y1; j <= y2 ;j++)
-            {
-                // 사각형의 변 위의 점
-                if (i == x1 || i == x2 || j == y1 || j == y2)
-                {
-                    board[i][j] = 1;
-                }
-            }
-        }
-        
-        for(int i = x1; i <=x2 ; i++)
-        {
-            for(int j = y1; j <= y2;j++)
-            {
-                // 사각형 내부
-                if ( (i > x1 && i <x2 )&& (j > y1 && j < y2) )
-                {
-                    board[i][j] = 2;
-                }
-            }
-        }
+        // 사각형 내부에 잇는가
+        if( a > rect[0] && a < rect[2] && b > rect[1] && b < rect[3]) return false;
+        // 내부에 없고 선 위에잇는가
+        else if ( (a == rect[0] || a == rect[2]) && b >= rect[1] && b <= rect[3]) isOnLine = true;
+        else if ( (b == rect[1] || b == rect[3]) && a >= rect[0] && a <= rect[2]) isOnLine = true;
     }
-    for(auto rect : rectangle)
-    {
-        int x1 = rect[0] * 2;
-        int y1 = rect[1] * 2;
-        int x2 = rect[2] * 2; 
-        int y2 = rect[3] * 2;
-        
-        for(int i = x1; i <=x2 ; i++)
-        {
-            for(int j = y1; j <= y2;j++)
-            {
-                // 사각형 내부
-                if ( (i > x1 && i <x2 )&& (j > y1 && j < y2) )
-                {
-                    board[i][j] = 2;
-                }
-            }
-        }
-    }
+    if(isOnLine)
+        return true;
+    else return false;
 }
 
 int solution(vector<vector<int>> rectangle, int characterX, int characterY, int itemX, int itemY) {
     int answer = 0;
+    characterX *= 2;
+    characterY *= 2;
+    itemX *= 2;
+    itemY *= 2;
+    queue<tuple<int, int, int>> q;
+    vector<vector<bool>> isVisit(101, vector<bool>(101, false));
     
-    vector<vector<int>> board(101, vector<int>(101, 0));
-    vector<vector<int>> dist(101, vector<int>(101, -1));
-
-    BoardSetting(rectangle, board);
-    BFS(board, dist, characterX*2, characterY*2, itemX*2, itemY*2);
-
-    answer = dist[itemX*2][itemY*2] /2 ;
+    for(int i =0; i <rectangle.size(); i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            rectangle[i][j] *= 2;
+        }
+    }
+    
+    q.push({characterX, characterY, 0});
+    isVisit[characterX][characterY]= true;
+    
+    while(!q.empty())
+    {
+        int curX = get<0>(q.front());
+        int curY = get<1>(q.front());
+        int curDist = get<2>(q.front());
+        q.pop();
+        
+        //cout << curX << " " << curY << endl;
+        
+        if (curX == itemX && curY == itemY)
+        {
+            answer = curDist/2;
+            break;
+        }
+        
+        for(int i = 0; i <4; i++)
+        {
+            int nx = curX + dx[i];
+            int ny = curY + dy[i];
+            
+            if(nx < 0 || ny < 0 || nx >= 101 || ny >= 101 ) continue;
+            
+            if (CanGo(rectangle, nx, ny) && isVisit[nx][ny] == false) 
+            {
+                q.push({nx, ny, curDist+1});
+                isVisit[nx][ny] = true;
+            }
+        }
+    }
+    
     return answer;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
- 기존 bfs처럼 상하좌우를 검사하면, 1칸떨어진 블럭사이를점프하거나 두께가 1칸인 직사각형을 통과하는 문제가 생긴다
-    => 좌표를 전부 2배로키워서 해결하고 답을 2로 나눈다. 
- 좌표마다 모든 직사각형을 가지고 검사하는것보다 bfs를 돌기전에 직사각형의 정보를 맵에 입력해놓는게 좋다
-*/
+// 조건식이 까다로운 문제
+// 직사각형의 두께가 1이될때 통과하는문제를 해결해야함
+// 좌표를 2배시켜서 해결한다
+// 사각형의 내부에 있지 않더라도, 모든 사각형의 외부에 잇는 경우를 세 줘야한다
+// => 사각형 내부에 존재하지 않으면서 적어도 하나 이상의 사각형의 변 위에 존재해야 한다
